fixed indexing error introduced when translating from fortrain (off by one)

standard.py
@@ -7,10 +7,10 @@ def atmosphere(alt):
     NTAB = 8 #number of entries in defining tables
 
     #Define defining tables
-    htab = [0.0, 11.0, 20.0, 32.0, 47.0, 51.0, 71.0, 84.852]
-    ttab = [288.15, 216.65, 216.65, 228.65, 270.65, 270.65, 214.65, 186.946]
-    ptab = [1.0, 2.233611E-1, 5.403295E-2, 8.5666784E-3, 1.0945601E-3, 6.6063531E-4, 3.9046834E-5, 3.68501E-6]
-    gtab = [-6.5, 0.0, 1.0, 2.8, 0.0, -2.8, -2.0, 0.0]
+    htab = [np.nan, 0.0, 11.0, 20.0, 32.0, 47.0, 51.0, 71.0, 84.852]
+    ttab = [np.nan, 288.15, 216.65, 216.65, 228.65, 270.65, 270.65, 214.65, 186.946]
+    ptab = [np.nan, 1.0, 2.233611E-1, 5.403295E-2, 8.5666784E-3, 1.0945601E-3, 6.6063531E-4, 3.9046834E-5, 3.68501E-6]
+    gtab = [np.nan, -6.5, 0.0, 1.0, 2.8, 0.0, -2.8, -2.0, 0.0]
 
     #Calculate
     h = alt*REARTH/(alt+REARTH) #convert to geopotential alt
@@ -27,7 +27,7 @@ def atmosphere(alt):
     tbase = ttab[i]
     deltah = h-htab[i]
     tlocal = tbase + tgrad * deltah
-    theta = tlocal/ttab[0]
+    theta = tlocal/ttab[1]
 
     if(tgrad == 0.0):
         delta = ptab[i] * np.exp(-1*GMR*deltah/tbase)